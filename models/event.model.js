e593d18468d0a50aa94b3cffc2ebddfce261325c
Sadra Amlashi <amlashi.sadra@gmail.com>
2021-04-08 13:29:59 +0430

    Add User model

--- models/event.model.js
+++ models/event.model.js
@@ -16,6 +16,10 @@ const eventSchema = new Schema({
     date: {
         type: Date,
         required: true
+    },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     }
 })
 
