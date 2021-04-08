e7d517135affde39b4fbfb42798146efc92b16cf
Sadra Amlashi <amlashi.sadra@gmail.com>
2021-04-08 12:47:05 +0430

    Add mongoose

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express')
 const {json} = require('body-parser')
 const { graphqlHTTP } = require('express-graphql');
 const {buildSchema} = require('graphql')
-
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.use(json())
+const Event = require('./models/event.model')
 
-const events = []
+app.use(json())
 
 const graphQlSchema = `
         type Event {
@@ -46,26 +47,46 @@ app.use('/graphql', graphqlHTTP({
     schema: buildSchema(graphQlSchema),
     rootValue: {
         events: () => {
-            return events
+            return Event
+                .find({})
+                .then(events => {
+                    return events.map( ev => {
+                        return {...ev._doc, _id: ev.id}
+                    })
+                })
+                .catch(err => {
+                    throw err
+                })
         },
         createEvent: (args) => {
             const {title, description, price, date} = args.eventInput;
 
-            const event = {
-                _id: new Date().getTime(),
+            const event = new Event({
                 title,
                 description,
                 price: +price,
-                date
-            }
-
-            events.push(event)
+                date: new Date(date)
+            })
 
             return event
+                .save()
+                .then(ev => {
+                    return {...ev._doc, _id: ev.id}
+                })
+                .catch(err => {
+                    throw err
+                })
         }
     },
     graphiql: true
 }))
 
-
-app.listen(PORT, () => console.log(`Server is up and running on http://localhost:${PORT}`))
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.zozvv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+        { useUnifiedTopology: true }
+    ).then(() => {
+        app.listen(PORT, () => console.log(`Server is up and running on http://localhost:${PORT}`))
+    }).catch(err => {
+        console.log(err)
+    })
---
+++ models/event.model.js
@@ -0,0 +1,22 @@
+const {Schema, model} = require('mongoose')
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+})
+
+module.exports = model('Event', eventSchema)
