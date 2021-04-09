ed78108a286bd6d43409a0e60d9b8687b9b282e1
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-04-09 22:41:24 +0430

    Fix auth issue

--- middlewares/authGuard.js
+++ middlewares/authGuard.js
@@ -5,31 +5,31 @@ module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization');
 
   if (!authHeader) {
-    return unAthorized();
+    return unAthorized(req, next);
   }
 
   const token = authHeader.splic(' ')[1]; //Bearer XXXXXX
 
   if (!token || token === '') {
-    return unAthorized();
+    return unAthorized(req, next);
   }
 
   try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
     if (!decodedToken) {
-      return unAthorized();
+      return unAthorized(req, next);
     }
 
     req.isAuth = true;
     req.userId = decodedToken.userId;
     return next();
   } catch (err) {
-    return unAthorized();
+    return unAthorized(req, next);
   }
 };
 
-function unAthorized() {
+function unAthorized(req, next) {
   req.isAuth = false;
   return next();
 }
